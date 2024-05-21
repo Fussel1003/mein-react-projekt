757bd6a24388f2d82358866524c68e8683966a5c
Kevin Böhning <ke.boehning@icloud.com>
2024-05-21 14:55:52 +0200

    .

--- src/index.js
+++ src/index.js
@@ -4,11 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let myname = "Anton"
+
+let CustomStyle = {
+  color: "grey",
+  fontSize: "40px",
+  border: "15px solid orange"
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <>
+  <h1 className="heading">Hallo ich bin der Kevin</h1>
+  
+  <h2 className='blue'> und ich bin der {myname}</h2>
+
+  <h3 className='heading'>Meine Lieblingszahl ist {Math.round(Math.random() *10)}</h3>
+
+ <background className="able">Hallo ich bin auch der Button </background>
+
+
+  <button id='5' className='violet'>Klick mich</button>
+  
+<h5 style={{color: "pink"}}>Faben Test</h5>
+  
+  <h4>Ich bin ein Wort </h4>
+
+  <h4 style={CustomStyle}>ich bin ein inline CSS </h4>
+  </>
+
+ // <React.StrictMode>
+ //   <App />
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
